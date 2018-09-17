2757ad788a964ae2016bcdf306647901fae80d0b
maxwellbenton <maxwellbenton@gmail.com>
2018-09-17 08:17:29 -0400

    updated solution rebase from master

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import zero from '../assets/stars/0-stars.png'
-import one from '../assets/stars/1-stars.png'
-import two from '../assets/stars/2-stars.png'
-import three from '../assets/stars/3-stars.png'
-import four from '../assets/stars/4-stars.png'
-import five from '../assets/stars/5-stars.png'
+import zero from '../assets/stars/0-stars.png';
+import one from '../assets/stars/1-stars.png';
+import two from '../assets/stars/2-stars.png';
+import three from '../assets/stars/3-stars.png';
+import four from '../assets/stars/4-stars.png';
+import five from '../assets/stars/5-stars.png';
 
-const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
+const imgMapper = { 0: zero, 1: one, 2: two, 3: three, 4: four, 5: five };
 
 export default class CardBack extends Component {
+	generateRatingElement = () => {
+		if (this.props.IMDBRating !== null) {
+			return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
+		}
+		return <h4>No Rating Found</h4>;
+	};
 
-  generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
-  }
-
-  render() {
-    return (
-      <div className="card-back">
-        <h3 className="title"></h3>
-        <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
-        <span />
-        <h5 className="genres"></h5>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="card-back">
+				<h3 className="title">{this.props.title}</h3>
+				<span />
+				{this.generateRatingElement()}
+				<span />
+				<h5 className="genres">{this.props.genres.join(', ')}</h5>
+			</div>
+		);
+	}
 }
--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 export default class CardFront extends Component {
-
-  render() {
-    return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div
+				className="card-front"
+				style={{ backgroundImage: `url(${this.props.poster})` }}
+			/>
+		);
+	}
 }
