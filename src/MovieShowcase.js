48ad5990277a5c851a571626e0c5f79b7279a779
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2019-10-29 17:40:27 -0400

    Update MovieShowcase.js

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,7 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    // map over your movieData array and return an array of the correct JSX
   }
 
   render() {
