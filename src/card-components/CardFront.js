20774ac47d829473f769b1f6f069c23c8c617676
github username <raustin.melchior@gmail.com>
2019-12-14 14:07:02 -0800

    remove prompt for creating rating element

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 
 export default class CardFront extends Component {
 
-  generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
-  }
-
   render() {
     return (
       <div className="card-front">
