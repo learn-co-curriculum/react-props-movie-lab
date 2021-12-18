9dd08112046dbf653e919bb7c183deded7907fe8
dsulliv4 <dsulliv4@binghamton.edu>
2021-12-18 17:46:49 -0500

    commit tree bb19a4c90c5065b5c1efd24b0dcbaf62481307cc

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,21 @@
+
+   
 import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import MovieCard from './card-components/MovieCard.js';
+import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
+	generateMovieCards = () => {
+		return movieData.map((movie, index) => <MovieCard
+		      key={index}
+		      title={movie.title}
+		      IMDBRating={movie.IMDBRating}
+		      genres={movie.genres}
+		      poster={movie.poster}
+		    />)
+	};
 
-  generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
-  }
-
-  render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
-  }
+	render() {
+		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
+	}
 }
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -1,43 +1,49 @@
-import defaultPoster from '../assets/poster-imgs/default.png'
-import cmi from '../assets/poster-imgs/choux-and-maru-go-to-istanbul.png'
-import cmp1 from '../assets/poster-imgs/choux-and-maru-p1.png'
-import cb from '../assets/poster-imgs/chromeboi.png'
-import efv from '../assets/poster-imgs/escape-from-vim.png'
-import goldeneye from '../assets/poster-imgs/goldeneye.jpg'
-import hbmc from '../assets/poster-imgs/handsome-boy-modeling-club.png'
-import msts from '../assets/poster-imgs/marus-spinoff-trapped-in-the-sheets.png'
-import tkr from '../assets/poster-imgs/terrance-king-of-the-rats.png'
-import ttm from '../assets/poster-imgs/the-trash-man.png'
+import defaultPoster from '../assets/poster-imgs/default.png';
+import cmi from '../assets/poster-imgs/choux-and-maru-go-to-istanbul.png';
+import cmp1 from '../assets/poster-imgs/choux-and-maru-p1.png';
+import cb from '../assets/poster-imgs/chromeboi.png';
+import efv from '../assets/poster-imgs/escape-from-vim.png';
+import goldeneye from '../assets/poster-imgs/goldeneye.jpg';
+import hbmc from '../assets/poster-imgs/handsome-boy-modeling-club.png';
+import msts from '../assets/poster-imgs/marus-spinoff-trapped-in-the-sheets.png';
+import tkr from '../assets/poster-imgs/terrance-king-of-the-rats.png';
+import ttm from '../assets/poster-imgs/the-trash-man.png';
 
 import React, { Component } from 'react';
 import CardFront from './CardFront.js';
 import CardBack from './CardBack.js';
 
-
 const posterMap = {
-  'choux-maru-istanbul': cmi,
-  'choux-maru-part-1': cmp1,
-  'chromeboi': cb,
-  'escape-from-vim': efv,
-  'goldeneye': goldeneye,
-  'handsome-boy': hbmc,
-  'marus-spinoff': msts,
-  'terrance-king': tkr,
-  'the-trash-man': ttm,
-  'default': defaultPoster
-}
+	'choux-maru-istanbul': cmi,
+	'choux-maru-part-1': cmp1,
+	chromeboi: cb,
+	'escape-from-vim': efv,
+	goldeneye: goldeneye,
+	'handsome-boy': hbmc,
+	'marus-spinoff': msts,
+	'terrance-king': tkr,
+	'the-trash-man': ttm,
+	default: defaultPoster
+};
 
 export default class MovieCard extends Component {
-
-  render() {
-    return (
-      <div className="movie-card">
-        {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="movie-card">
+				<CardFront poster={posterMap[this.props.poster]} />
+				<CardBack
+					title={this.props.title}
+					IMDBRating={this.props.IMDBRating}
+					genres={this.props.genres}
+				/>
+			</div>
+		);
+	}
 }
 
-// Don't forget your default props!
+MovieCard.defaultProps = {
+	title: 'Unknown',
+	IMDBRating: null,
+	genres: ['No Genre(s) Found'],
+	poster: 'default'
+};
